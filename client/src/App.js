50d66f961518a2165afc95c4e10d68f370b39297
Janessa Garrow <janessa.garrow@gmail.com>
2020-01-25 10:24:52 -0700

    Fetch player data, set to state in componentDidMount. Display data in cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { Component } from "react";
+import styled from "@emotion/styled";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled.div`
+    width: 80%;
+    max-width: 960px;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+const Card = styled.div`
+    width: 300px;
+    box-sizing: border-box;
+    border-radius: 8px;
+    padding: 1rem 0.5rem;
+    margin: 10px;
+    box-shadow: 0px 0px 5px 0px lightgrey;
+`;
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 150px 1fr;
+    grid-gap: 10px;
+`;
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            players: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:5000/api/players")
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                this.setState({ ...this.state, players: res });
+            })
+            .catch(err => console.log("Error fetching data: ", err));
+    }
+
+    render() {
+        return (
+            <AppContainer>
+                {this.state.players !== [] &&
+                    this.state.players.map(player => (
+                        <Card key={player.id}>
+                            <h2 css={{ textAlign: "center" }}>{player.name}</h2>
+                            <Grid>
+                                <p
+                                    css={{
+                                        justifySelf: "flex-end"
+                                    }}
+                                >
+                                    Player country:
+                                </p>
+                                <p>{player.country}</p>
+                                <p
+                                    css={{
+                                        justifySelf: "flex-end"
+                                    }}
+                                >
+                                    # internet searches:
+                                </p>
+                                <p>{player.searches}</p>
+                            </Grid>
+                        </Card>
+                    ))}
+            </AppContainer>
+        );
+    }
 }
 
 export default App;
